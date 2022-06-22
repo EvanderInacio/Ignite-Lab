9cb60a8005a2e54c39abb204dce2ffcc1858c80a
Evander Inacio <evander.20112@hotmail.com>
2022-06-22 19:48:14 -0300

    feat: :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
-import { useCallback, useEffect } from 'react'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
-  )
+    <Event />
+  );
 }
 
 export default App
