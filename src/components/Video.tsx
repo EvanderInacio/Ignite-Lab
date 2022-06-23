841d14935778492e6f670d3da07df86540f4c640
Evander Inacio <evander.20112@hotmail.com>
2022-06-23 19:11:45 -0300

    refactor: :memo:

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,25 +1,94 @@
-import { DiscordLogo, Lightning } from 'phosphor-react'
+import { DefaultUi, Player, Youtube } from '@vime/react'
+import { gql, useQuery } from '@apollo/client';
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react'
 
-export function Video() {
+import '@vime/core/themes/default.css';
+
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        avatarURL
+        bio
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      avatarURL: string;
+      bio: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    }
+  }) 
+
+  if(!data) {
+    return (
+      <div className='flex-1'>
+        <p>Carregando aguarde.......</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16 ">
           <div className="flex-1">
             <h1 className="text-2xl font-bold ">
-              Aula 01 - Abertura do Ignite Lab
+              {data.lesson.title}
             </h1>
-            <p className="mt-4 text-gray-200">
-              Nessa aula vamos dar início ao projeto criando a estrutura base da
-              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
-              realizar o setup do nosso projeto no GraphCMS criando as entidades
-              da aplicação e integrando a API GraphQL gerada pela plataforma no
-              nosso front-end utilizando Apollo Client.
+            <p className="mt-4 text-gray-200 leading-relaxed">
+             {data.lesson.description}
             </p>
+
+            <div className='flex items-center gap-4 mt-6'>
+              <img
+               className="h-16 w-16 rounded-full border-2 border-blue-500" 
+               src={data.lesson.teacher.avatarURL}
+               alt="imagem de perfil" 
+              />
+
+              <div className='leading-relaxed'>
+                 <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                 </strong>
+                 <span className='text-gray-200 text-sm block'>
+                  {data.lesson.teacher.bio}   
+                 </span>
+              </div>
+            </div>
           </div>
 
           <div className="flex flex-col gap-4 ">
@@ -41,7 +110,44 @@ export function Video() {
           </div>
         </div>
 
-        <div></div>
+        <div className='gap-8 mt-20 grid grid-cols-2'>
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 translate-colors">
+            <div className='bg-green-700 h-full p-6 flex items-center'>
+              <FileArrowDown size={40}/>
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'>
+                Material complementar
+              </strong>
+              <p className='text-sm text-gray-200 mt-2'>
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'> 
+             <CaretRight size={24}/>
+            </div>
+          </a>
+
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 translate-colors">
+            <div className='bg-green-700 h-full p-6 flex items-center'>
+              <FileArrowDown size={40}/>
+            </div>
+            <div className='py-6 leading-relaxed'>
+              <strong className='text-2xl'>
+                Wallpapers exclusivos
+              </strong>
+              <p className='text-sm text-gray-200 mt-2'>
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+              </p>
+            </div>
+            <div className='h-full p-6 flex items-center'> 
+             <CaretRight size={24}/>
+            </div>
+          </a>
+          
+
+        </div>
+
       </div>
     </div>
   )
