3ed654f38ef1f2de0580b5c530b901e650c6069e
Evander Inacio <evander.20112@hotmail.com>
2022-06-24 14:55:53 -0300

    feat: :rocket:

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,37 @@
+import { gql, useMutation } from '@apollo/client';
+import { useState, FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { Logo } from '../components/Logo'
 
+const CREATE_SUBSCRIBE_MUTATION = gql `
+  mutation CreateSubscriber ($name: String!, $email: String!) {
+    createSubscriber(data: {name: $name, email: $email}) {
+      id
+    }
+  }
+`
+
 export function Subscriber() {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBE_MUTATION)
+
+  async function handleSubscriber(event: FormEvent){
+    event.preventDefault();
+
+    await createSubscriber({
+      variables: {
+        name,
+        email,
+      } 
+    })
+
+    navigate('/event')
+  } 
+
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="z-20 w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto relative mb-48">
@@ -24,33 +55,41 @@ export function Subscriber() {
             Inscreva-se gratuitamente
           </strong>
 
-          <form action="" className="flex flex-col gap-2 w-full">
+          <form onSubmit={handleSubscriber} className="flex flex-col gap-2 w-full">
             <input
               className="bg-gray-900 rounded px-5 h-14"
               type="text"
               placeholder="Seu nome completo"
+              onChange={event => setName(event.target.value)}
             />
             <input
               className="bg-gray-900 ro px-5 h-14"
               type="email"
               placeholder="Digite seu e-mail"
+              onChange={event => setEmail(event.target.value)}
             />
 
             <button
               type="submit"
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 translate-colors"
+              disabled={loading}
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 translate-colors disabled:opacity-50"
             >
               Garantir minha vaga
             </button>
           </form>
         </div>
       </div>
+
       <img
         src="/src/assets/ReactJS-icon.png"
-        className="z-10 absolute top-5"
+        className="z-10 fixed top-5"
         alt=""
       />
-      <img src="/src/assets/code-mockup.png" className="mt-2" alt="" />
+      <img 
+        src="/src/assets/code-mockup.png" 
+        className="mt-2 z-20"
+        alt=""
+       />
     </div>
   )
 }
